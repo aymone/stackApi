51b54044441e497736bb7708985b8439ba60f583
Marcelo Aymone <mbaymone@gmail.com>
2015-07-26 20:31:12 -0300

    Bind dos parametros de busca

--- src/StackMoblee/Repository/QuestionRepository.php
+++ src/StackMoblee/Repository/QuestionRepository.php
@@ -15,7 +15,12 @@ use Doctrine\Common\Collections\ArrayCollection;
 
 class QuestionRepository extends EntityRepository
 {
-
+    /**
+     * Manage get query for api
+     * Note:
+     * @param $params
+     * @return array|bool
+     */
     public function query($params) {
 
         $fields = [
@@ -23,10 +28,15 @@ class QuestionRepository extends EntityRepository
             'q.creation_date', 'q.link', 'q.is_answered'
         ];
 
+        if (!in_array('q.' . $params['sort'], $fields)) {
+            return null;
+        }
+
         $q = $this->createQueryBuilder('q')
             ->select($fields)
-            ->where('q.score > ' . $params['score'])
-            ->orderBy('q.' . $params['sort'], 'ASC');
+            ->where('q.score > :score')
+            ->orderBy('q.' . $params['sort'], ' ASC')
+            ->setParameter(':score', $params['score']);
 
         return (new Paginator($q, false))
             ->getQuery()
